69369bd7107f3a0e27ca3990ba4b30bd9a313f8a
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-26 16:30:44 +0100

    keep testing

--- second-snack/for/jsnack2.js
+++ second-snack/for/jsnack2.js
@@ -2,23 +2,19 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 // creo un array con gli invitati alla festa
-const Invitati = ["Cesare", "Massimo", "Lucio", "Pinco", "Pallo"];
+const Invitati = ["Cesare", "Massimo", "Lucio", "Pinco", "Marco"];
 // console.log(Invitati);
 
 // creo un prompt in cui chiedo il nome all'utente
 let Nome = prompt('Inserisca il nome');
-console.log('il tuo nome', Nome);
-
-// creo una variabile boleana per avere conferma nel ciclo
-// let find = false;
+console.log('il tuo nome è:', Nome);
 
 // creo un ciclo for per controllare tutta la lista degli invitati
 for (i = 0; i < Invitati.length; i++) {
-  // console.log(Invitati[i]);
-  // se il nome che inserisce l'utente è uguale ad uno di quelli nella lista e allora può partecipare alla festa
-  if (Nome = Invitati[i]) {
-    
-    
+  console.log(Invitati[i]);
+  // se il nome che inserisce l'utente è uguale ad uno di quelli nella lista, allora stampo il mex: puoi partecipare alla festa
+  if (Invitati[i] == Nome) {
+    alert('Sei ammesso alla festa');
   }
 }
 
