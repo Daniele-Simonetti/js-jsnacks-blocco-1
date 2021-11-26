0f1aade0f266bd6d0eaa88411a563be2c95b7295
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-26 16:50:14 +0100

    end 2 snack with for

--- second-snack/for/jsnack2.js
+++ second-snack/for/jsnack2.js
@@ -2,19 +2,30 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 // creo un array con gli invitati alla festa
-const Invitati = ["Cesare", "Massimo", "Lucio", "Pinco", "Marco"];
+const invitati = ["Cesare", "Massimo", "Lucio", "Pinco", "Marco"];
 // console.log(Invitati);
 
 // creo un prompt in cui chiedo il nome all'utente
-let Nome = prompt('Inserisca il nome');
-console.log('il tuo nome è:', Nome);
+let nome = prompt('Inserisca il nome');
+console.log('il tuo nome è:', nome);
+
+// creo la variabile boleana per dare un freno al ciclo
+let find = false;
 
 // creo un ciclo for per controllare tutta la lista degli invitati
-for (i = 0; i < Invitati.length; i++) {
-  console.log(Invitati[i]);
-  // se il nome che inserisce l'utente è uguale ad uno di quelli nella lista, allora stampo il mex: puoi partecipare alla festa
-  if (Invitati[i] == Nome) {
-    alert('Sei ammesso alla festa');
-  }
+for (i = 0; i < invitati.length; i++) {
+  // console.log(Invitati[i]);
+  // all'interno del ciclo controllo che il nome sia presente nella lista
+  if (nome == invitati[i]) {
+    find = true;
+  } 
 }
+
+// se il nome è presente e quindi la variabile boleana diventa true allora stampo un alert, altrimenti ne stampo un altro
+if (find == true) {
+  alert('sei ammesso alla festa');
+} else {
+  alert(' non puoi entrare');
+}
+
 
